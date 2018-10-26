71c8a4ca660de00400a37111b450cfde72c5b389
Xiaoting Fu <xfu7@ncsu.edu>
2018-10-26 16:17:14 -0400

    update preprocess file

--- s1/prepare_data.py
+++ s1/prepare_data.py
@@ -4,28 +4,79 @@ import csv
 # utt-id prompt   wav-filename transcription label1 label2 
 
 # Directories
-1st_data_dir='/home/xfu7/data/sharedTask1st'
-2nd_data_dir='/home/xfu7/data/sharedTask2nd'
+data_dir_1st='/home/xfu7/data/sharedTask1st'
+data_dir_2nd='/home/xfu7/data/sharedTask2nd'
 
 # Labels
-1st_test_label=1st_data_dir+'/label/scst1_testData_annotated.csv'
-1st_train_label=1st_data_dir+'/label/scst1_trainingData_speechTask.csv'
+test_label_1st=data_dir_1st+'/label/scst1_testData_annotated.csv'
+train_label_1st=data_dir_1st+'/label/scst1_trainingData_speechTask.csv'
 
-2nd_train_label_A=2nd_data_dir+'/train/scst2_training_data_A_speech.csv'
-2nd_train_label_B=2nd_data_dir+'/train/scst2_training_data_B_speech.csv'
-2nd_train_label_C=2nd_data_dir+'/train/scst2_training_data_C_speech.csv'
-2nd_test_label=2nd_data_dir+'/test/scst2_testDataSpeech.csv'
+train_label_A_2nd=data_dir_2nd+'/train/scst2_training_data_A_speech.csv'
+train_label_B_2nd=data_dir_2nd+'/train/scst2_training_data_B_speech.csv'
+train_label_C_2nd=data_dir_2nd+'/train/scst2_training_data_C_speech.csv'
+test_label_2nd=data_dir_2nd+'/test/scst2_testDataSpeech.csv'
 
 # Audio files
 
-1st_audio=1st_data_dir+'/audio'
-2nd_train_audio=2nd_data_dir+'/train/TrainingDataWavfiles'
-2nd_test_audio=2nd_data_dir+'/test/audio'
+audio_1st=data_dir_1st+'/audio'
+train_audio_2nd=data_dir_2nd+'/train/TrainingDataWavfiles'
+test_audio_2nd=data_dir_2nd+'/test/audio'
 
+
+destination_train='/home/xfu7/kaldi/egs/st/s1/data/sharedTask2nd/all/'
+destination_test='/home/xfu7/kaldi/egs/st/s1/data/sharedTask2nd_test/'
 
 '''
 prepare necessary kaldi data for training
 '''
-def kaldi_prepare_data():
-	
+def prepareKaldiData(prefix, csvPath,audioPath, dest):
+    # Generating Kaldi format files for ASR training
+    textFile ='text'
+    wav_scpFile ='wav.scp'
+    utt2spkFile ='utt2spk'
+    spk2uttFile ='spk2utt'
+    print("========================Process CSV=====================")
+    with open(csvPath) as csvfile:
+            readCSV = csv.reader(csvfile, delimiter=',')
+            for row in readCSV:
+                utt_id = prefix + '-' + str(row[0])
+                wave_file = str(row[2])
+                utterance = str(row[3])
 
+                print( "utt_id: "+ utt_id)
+                # Prepare text alignment file
+                # File: text
+                # utt_id    WORD1 WORD2 WORD3 WORD4 ...
+                text_line = utt_id +' '+ utterance+'\n'
+                with open(textFile, 'a') as out:
+                    out.write( text_line)
+
+                # File: wav.scp
+                # file_id    path/file
+                wavefile_path=audioPath+'/'+wave_file
+                wav_scp_line= utt_id + ' ' + wavefile_path +'\n'
+                # print('3. wav.scp : ' + wav_scp_line)
+                with open(wav_scpFile, 'a') as out:
+                    out.write(wav_scp_line)
+                    
+                # File: utt2spk
+                # utt_id    spkr
+                utt2spk_line = utt_id +' '+ utt_id+'\n'
+                print('5. utt2spk : ' + utt2spk_line)
+                with open(utt2spkFile, 'a') as out:
+                    out.write(utt2spk_line)
+                with open(spk2uttFile, 'a') as out:
+                    out.write(utt2spk_line)
+
+
+def main():
+    prepareKaldiData('sc1',train_label_1st, audio_1st, destination_train)
+    prepareKaldiData('sc1',test_label_1st, audio_1st, destination_train)
+    prepareKaldiData('sc2',train_label_A_2nd, train_audio_2nd,destination_train)
+    prepareKaldiData('sc2',train_label_B_2nd, train_audio_2nd,destination_train)
+    prepareKaldiData('sc2',train_label_C_2nd, train_audio_2nd,destination_train)
+    prepareKaldiData('sc2',test_label_2nd, test_audio_2nd,destination_test)
+
+
+
+main()
