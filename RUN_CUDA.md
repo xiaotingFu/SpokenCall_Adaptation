234554935a577a3588e578b4e7683110fc75139a
Xiaoting Fu <xfu7@ncsu.edu>
2018-11-12 09:17:13 -0500

    new preparting scripts

--- RUN_CUDA.md
+++ RUN_CUDA.md
@@ -7,3 +7,10 @@ Copyright (c) 2005-2016 NVIDIA Corporation
 Built on Tue_Jan_10_13:22:03_CST_2017
 Cuda compilation tools, release 8.0, V8.0.61
 
+cd /pvfs2/xfu7@oss-storage-0-108/pvfs2
+
+
+steps/decode_fmllr.sh --nj 15 --cmd run.pl --mem 28G --config conf/decode.conf --acwt 0.05 exp2/tri4a/graph_st2.o3g.kn.pr1-7 data/sharedTask2nd_test exp2/tri4a/decode_st.test_st2.o3g.kn.pr1-7_acwt0.05
+
+
+cmake .. -DBUILD_PYTHON=TRUE -DWITH_ZMQ=FALSE -WITH_MKL=FALSE
--- s1/prepare_data.py
+++ s1/prepare_data.py
@@ -74,7 +74,7 @@ def prepareKaldiTestData(prefix, dest):
             for row in readCSV:
                 if row_number!=0:
                     utt_id = prefix + '_' + str(row[0])
-                    utterance = str(row[3])
+                    utterance = processUtterance(str(row[3]) 
                     # Prepare text alignment file
                     # File: text
                     # utt_id    WORD1 WORD2 WORD3 WORD4 ...
@@ -84,7 +84,73 @@ def prepareKaldiTestData(prefix, dest):
                         out.write(text_line)
                 row_number=row_number+1
 
-    
+def prepareKaldiTestData_textonly(prefix, dest):
+    # prepare utterance
+    # prepare wavefile
+    textFile = dest+'/text'
+    # wav_scpFile =dest+'/wav.scp'
+    # utt2spkFile =dest+'/utt2spk'
+    # spk2uttFile =dest+'/spk2utt'
+    # audioPath = test_audio_2nd
+
+    # print("========================Process CSV WAV=====================")
+    # print("csvpath: " + test_label_2nd_wav)
+    # print("audiopath: " + audioPath)
+    # print("destination: " + dest)
+
+    # print("========================Output file========================")
+    # print("text: " + textFile)
+    # print("wav.scp" + wav_scpFile)
+
+    row_number=0
+    # with open(test_label_2nd_wav) as csvfile:
+    #         readCSV = csv.reader(csvfile, delimiter='\t')
+    #         for row in readCSV:
+    #             if row_number!=0:
+    #                 utt_id = prefix+str(row[0]) 
+    #                 wave_file = str(row[2])
+    #                 # File: wav.scp
+    #                 # file_id    path/file
+    #                 wavefile_path=audioPath+'/'+wave_file
+    #                 wav_scp_line= utt_id + ' ' + wavefile_path +'\n'
+    #                 with open(wav_scpFile, 'a+') as out:
+    #                     out.write(wav_scp_line)
+                    
+    #                 # File: utt2spk and spk2utt
+    #                 # utt_id    spkr
+    #                 utt2spk_line = utt_id +' '+ utt_id+'\n'
+    #                 with open(utt2spkFile, 'a+') as out:
+    #                     out.write(utt2spk_line)
+    #                 with open(spk2uttFile, 'a+') as out:
+    #                     out.write(utt2spk_line)
+    #             row_number=row_number+1
+    # row_number=0
+    print("========================Process CSV UTT=====================")
+    print("csvpath: " + test_label_2nd_utt)
+    with open(test_label_2nd_utt) as csvfile:
+            readCSV = csv.reader(csvfile, delimiter='\t')
+            for row in readCSV:
+                if row_number!=0:
+                    utt_id = prefix + '_' + str(row[0])
+                    utterance = processUtterance(str(row[3]) 
+                    # Prepare text alignment file
+                    # File: text
+                    # utt_id    WORD1 WORD2 WORD3 WORD4 ...
+                    text_line = utt_id + ' ' + utterance + '\n'
+                    # print( "1. text: "+ text_line)
+                    with open(textFile, 'a+') as out:
+                        out.write(text_line)
+                row_number=row_number+1
+def processUtterance(utt): 
+    new_utt = utt
+    if "silence" in utt:
+        new_utt.replace("silence","<sil>")
+    elif "xxx" in utt:
+        new_utt.replace("xxx","<unk>")
+    elif "*" in utt:
+        new_utt.replace("*","")
+    return new_utt.upper()
+      
 def prepareKaldiData(prefix, csvPath,audioPath, dest):
     # Generating Kaldi format files for ASR training
     textFile = dest+'/text'
@@ -103,7 +169,7 @@ def prepareKaldiData(prefix, csvPath,audioPath, dest):
                 if row_number!=0:
                     utt_id = prefix+str(row[0])
                     wave_file = str(row[2])
-                    utterance = str(row[3])
+                    utterance = processUtterance(str(row[3]))
 
                     # Prepare text alignment file
                     # File: text
@@ -131,13 +197,56 @@ def prepareKaldiData(prefix, csvPath,audioPath, dest):
                         out.write(utt2spk_line)
                 row_number=row_number+1
 
+def prepareKaldiData_textonly(prefix, csvPath,audioPath, dest):
+    # Generating Kaldi format files for ASR training
+    textFile = dest+'/text'
+    # wav_scpFile =dest+'/wav.scp'
+    # utt2spkFile =dest+'/utt2spk'
+    # spk2uttFile =dest+'/spk2utt'
 
+    print("========================Process CSV=====================")
+    print("csvpath: " + csvPath)
+    print("audiopath: " + audioPath)
+    print("destination: " + dest)
+    row_number=0
+    with open(csvPath) as csvfile:
+            readCSV = csv.reader(csvfile, delimiter='\t')
+            for row in readCSV:
+                if row_number!=0:
+                    utt_id = prefix+str(row[0])
+                    wave_file = str(row[2])
+                    utterance = processUtterance(str(row[3]))
+
+                    # Prepare text alignment file
+                    # File: text
+                    # utt_id    WORD1 WORD2 WORD3 WORD4 ...
+                    text_line = utt_id +' '+ utterance+'\n'
+                    # print( "1. text: "+ text_line)
+                    with open(textFile, 'a+') as out:
+                        out.write(text_line)
+
+                    # # File: wav.scp
+                    # # file_id    path/file
+                    # wavefile_path=audioPath+'/'+wave_file
+                    # wav_scp_line= utt_id + ' ' + wavefile_path +'\n'
+                    # # print('2. wav.scp : ' + wav_scp_line)
+                    # with open(wav_scpFile, 'a+') as out:
+                    #     out.write(wav_scp_line)
+                        
+                    # # File: utt2spk
+                    # # utt_id    spkr
+                    # utt2spk_line = utt_id +' '+ utt_id+'\n'
+                    # # print('3. utt2spk : ' + utt2spk_line)
+                    # with open(utt2spkFile, 'a+') as out:
+                    #     out.write(utt2spk_line)
+                    # with open(spk2uttFile, 'a+') as out:
+                    #     out.write(utt2spk_line)
+                row_number=row_number+1
 def main():
-    prepareKaldiData('01',train_label_1st, audio_1st, destination_train)
-    prepareKaldiData('01',test_label_1st, audio_1st, destination_train)
-    prepareKaldiData('02',train_label_A_2nd, train_audio_2nd,destination_train)
-    prepareKaldiData('02',train_label_B_2nd, train_audio_2nd,destination_train)
-    prepareKaldiData('02',train_label_C_2nd, train_audio_2nd,destination_train)
-    # prepareKaldiTestData('sc2', destination_test)
-    # prepareKaldiData('sc2',test_label_2nd, test_audio_2nd,destination_test)
+    prepareKaldiData_textonly('01',train_label_1st, audio_1st, destination_train)
+    prepareKaldiData_textonly('01',test_label_1st, audio_1st, destination_train)
+    prepareKaldiData_textonly('02',train_label_A_2nd, train_audio_2nd,destination_train)
+    prepareKaldiData_textonly('02',train_label_B_2nd, train_audio_2nd,destination_train)
+    prepareKaldiData_textonly('02',train_label_C_2nd, train_audio_2nd,destination_train)
+    prepareKaldiTestData_textonly('sc2', destination_test)
 main()
