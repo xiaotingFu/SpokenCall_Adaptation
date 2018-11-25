25cf39a64420f316e41ab13e358db1bb3502cf14
Xiaoting Fu <xfu7@ncsu.edu>
2018-11-25 14:34:01 -0500

    Update prepare_subset.sh

--- s1/prepare_subset.sh
+++ s1/prepare_subset.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 #Usage: combine_data.sh [--extra-files 'file1 file2'] <dest-data-dir> <src-data-dir1> <src-data-dir2> ...
-rm -rf data/ihm_8kHz/train_5k data/sharedTask2nd/train_5k data/ihm_8kHz/train_5k
+
+
+rm -rf data/ihm_s5/train_5k data/sharedTask2nd/train_5k data/ihm_8kHz/train_5k
 
 # get a subset of 5k from ihm dataset
-utils/subset_data_dir.sh data/ihm_8kHz/train 5000 data/ihm_8kHz/train_5k
+utils/subset_data_dir.sh data/ihm_s5/train 10000 data/ihm_s5/train_10k
+utils/subset_data_dir.sh data/ihm_s5/train 12000 data/ihm_s5/train_12k
+utils/subset_data_dir.sh data/ihm_s5/train 20000 data/ihm_s5/train_20k
+utils/subset_data_dir.sh data/ihm_s5/train 50000 data/ihm_s5/train_50k
 
 # get a subset of 5K from ST12 dataset
 utils/subset_data_dir.sh data/sharedTask2nd/all 5000 data/sharedTask2nd/train_5k
