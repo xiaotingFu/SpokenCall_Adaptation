e6bd331be99ac7e6cb06932274b26e56415a2804
Xiaoting Fu <xfu7@ncsu.edu>
2018-11-25 20:13:53 -0500

    Update prepare_subset.sh

--- s1/prepare_subset.sh
+++ s1/prepare_subset.sh
@@ -2,23 +2,27 @@
 
 #Usage: combine_data.sh [--extra-files 'file1 file2'] <dest-data-dir> <src-data-dir1> <src-data-dir2> ...
 
+st_dir=data/sharedTask2nd
+ihm_dir=data/ihm
+dest_dir=data/st_ihm_all
 
-rm -rf data/ihm_s5/train_5k data/sharedTask2nd/train_5k data/ihm_8kHz/train_5k
-
-# get a subset of 5k from ihm dataset
-utils/subset_data_dir.sh data/ihm_s5/train 10000 data/ihm_s5/train_10k
-utils/subset_data_dir.sh data/ihm_s5/train 12000 data/ihm_s5/train_12k
-utils/subset_data_dir.sh data/ihm_s5/train 20000 data/ihm_s5/train_20k
-utils/subset_data_dir.sh data/ihm_s5/train 50000 data/ihm_s5/train_50k
+# Clear the destination directory
+rm -rf $dest_dir/*
+rm -rf data/ihm_s5/train_5k $st_dir/train_5k 
+# get a list of subset of ihm dataset 
+utils/subset_data_dir.sh $ihm_dir/train 10000 $ihm_dir/train_10k
+utils/subset_data_dir.sh $ihm_dir/train 12000 $ihm_dir/train_12k
+utils/subset_data_dir.sh $ihm_dir/train 20000 $ihm_dir/train_20k
+utils/subset_data_dir.sh $ihm_dir/train 50000 $ihm_dir/train_50k
 
 # get a subset of 5K from ST12 dataset
-utils/subset_data_dir.sh data/sharedTask2nd/all 5000 data/sharedTask2nd/train_5k
+utils/subset_data_dir.sh $st_dir/all 5000 $st_dir/train_5k
 
 # combine these two subsets
-utils/combine_data.sh data/st_ihm_all/train_5S10I data/sharedTask2nd/train_5k data/ihm_s5/train_10k 
-utils/combine_data.sh data/st_ihm_all/train_5S20I data/sharedTask2nd/train_5k data/ihm_s5/train_20k 
-utils/combine_data.sh data/st_ihm_all/train_12S12I data/sharedTask2nd/all data/ihm_s5/train_12k 
-utils/combine_data.sh data/st_ihm_all/train_12S50I data/sharedTask2nd/all data/ihm_s5/train_50k 
-utils/combine_data.sh data/st_ihm_all/train_all data/sharedTask2nd/all data/ihm_s5/train
+utils/combine_data.sh $dest_dir/train_5S10I $st_dir/train_5k $ihm_dir/train_10k 
+utils/combine_data.sh $dest_dir/train_5S20I $st_dir/train_5k $ihm_dir/train_20k 
+utils/combine_data.sh $dest_dir/train_12S12I $st_dir/all $ihm_dir/train_12k 
+utils/combine_data.sh $dest_dir/train_12S50I $st_dir/all $ihm_dir/train_50k 
+utils/combine_data.sh $dest_dir/train_all $st_dir/all $ihm_dir/train
 
 
